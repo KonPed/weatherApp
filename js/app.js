9b2eeb05eaaae34dd83a7651518143e1170189f5
Paidarakis Konstantinos <dekoza1987@gmail.com>
2016-02-21 20:26:17 +0200

    add the js file,make the AJAX call,convert the icons from the json data

---
+++ js/app.js
@@ -0,0 +1,29 @@
+var icons = {
+  "clear-day": "B",
+  "clear-night": "C",
+  rain: "R",
+  snow: "W",
+  sleet: "X",
+  wind: "S",
+  fog: "L",
+  cloudy: "Y",
+  "partly-cloudy-day": "H",
+  "partly-cloudy-night": "3"
+}
+
+function loadWeather() {
+  var forecastUrl = "https://api.forecast.io/forecast/b5f1fcd0a6f50029f63cdce97b53d747/37.8167,23.7833";
+  $.ajax({
+    url: forecastUrl,
+    dataType: "jsonp",
+    method: "POST",
+    success: function(response) {
+      $("#current_temp").html(Math.round(response.currently.temperature) + "&deg;F");
+      $("#current_summary").text(response.currently.summary);
+      $("#current_temp").attr("data-icon", icons[response.currently.icon]);
+    },
+    error: function(err) {
+      console.log("err");
+    }
+  });
+}
